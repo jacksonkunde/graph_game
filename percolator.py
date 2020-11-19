52ae572ea0ec0461a43003c3bca686ac4bb28ac8
jacksonkunde <54416292+jacksonkunde@users.noreply.github.com>
2020-11-19 10:28:32 -0800

    added percolator.py

---
+++ percolator.py
@@ -0,0 +1,17 @@
+class PercolationPlayer:
+	# `graph` is an instance of a Graph, `player` is an integer (0 or 1).
+# Should return a vertex `v` from graph.V where v.color == -1
+def ChooseVertexToColor(graph, player):
+    pass
+
+# `graph` is an instance of a Graph, `player` is an integer (0 or 1).
+# Should return a vertex `v` from graph.V where v.color == player
+def ChooseVertexToRemove(graph, player):
+    pass
+
+# Feel free to put any personal driver code here.
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
